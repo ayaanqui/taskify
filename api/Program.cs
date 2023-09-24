6eb2261979ae4440434ea6cc50b442d677cefb8b
ayaanqui <ayaanqui.com@gmail.com>
2023-09-24 16:43:59 -0500

    feat: setup jwt

--- api/Program.cs
+++ api/Program.cs
@@ -1,20 +1,39 @@
+using System.Text;
 using api.Database;
+using api.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddScoped<UserRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication()
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateActor = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        };
+    })
     .AddGoogle(options =>
     {
         var section = builder.Configuration.GetSection("Authentication:Google");
         options.ClientId = section["ClientId"];
         options.ClientSecret = section["ClientSecret"];
+        options.CallbackPath = "/oauth/google/callback";
     });
 
 var app = builder.Build();
